951f9625716d2c0f72ff324cbb231d486b961ab6
Juan Manuel Geny <juanmanuel.geny@gmail.com>
2018-03-03 07:51:46 -0300

    inicio

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Geny Paratriatlon</title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <link rel="shortcut icon" href="img/geny.ico" />
+    
+    {{-- bootstrap --}}
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    {{-- iconos --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header class="container-fluid text-center header-1">
+        <img src="img/GenyParatriatlonTransparente.png" alt="">        
+    </header>
+
+    <section class="container principal">
+                <div class="links social-media text-center">
+                    <a href="https://www.facebook.com/juanmageny" class="fa fa-facebook" target="new"></a>
+                    <a href="https://twitter.com/paratriatlon" class="fa fa-twitter" target="new"></a>
+                    <a href="https://www.linkedin.com/in/jmgeny/" class="fa fa-linkedin" target="new"></a>
+                    <a href="https://www.instagram.com/juanmanuelgeny/" class="fa fa-instagram" target="new"></a>
+                </div>        
+    </section>
+    <section class="container text-center">
+                <div class="link-portada" ">
+                    <a href="https://www.triathlon.org/athletes/profile/22141/juan_manuel_geny"><h3>Perfil ITU</h3></a>
+                </div> 
+                <div class="link-portada" ">
+                    <a href="mailto:juanmanuel.geny@gmail.com" 
+                    target="new"><h3>juanmanuel.geny@gmail.com</h3></a>
+                </div>
+    </section>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>    
+</body>
+</html>
