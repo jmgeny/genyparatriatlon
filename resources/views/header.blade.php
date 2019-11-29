b8c7615a336714f95d7695302b0a31be9800f5c0
JuanManuelGeny <juanmanuel.geny@gmail.com>
2019-11-29 18:16:05 -0300

    dfd

--- resources/views/header.blade.php
+++
@@ -1,7 +0,0 @@
-    <header class="masthead text-white text-center">
-      <div class="container">
-        <img class="img-fluid mb-5 d-block mx-auto" src="img/GenyParatriatlonTransparente.png" alt="">
-        <!-- <h1 class="text-uppercase mb-0">Geny Paratriatlon</h1> -->
-        @include('redes')
-      </div>
-    </header>
