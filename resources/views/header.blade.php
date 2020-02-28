3b813fc2c55d54272f4a13f9f668a0aa73e68ac6
JuanManuelGeny <juanmanuel.geny@gmail.com>
2020-02-28 17:04:36 -0300

    Proxima Carrera

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,6 @@
 <header class="cabeza">
 	<div class="title">
-		<img src="img/GenyParatriatlonTransparente.png" alt="">
+		<img src="img/GenyParatriatlonTransparente.png" alt="logo">
 	</div>
 		@include('redes')
 </header>	
--- resources/views/listaCarreras.blade.php
+++ resources/views/listaCarreras.blade.php
@@ -1,26 +1,38 @@
 <section class="container">
   <div class="row">
+    <div class="col-md-12">
+      <div class="media">
+        <img src="img/maraton.png" class="mr-3" alt="maraton">
+        <div class="media-body">
+          <h5 class="mt-0"><strong>Proxima Carrera</strong></h5>
+          <a href="https://maratondemendoza.com/" target="new">Maraton de Mendoza 10km</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <hr>
+  <div class="row">
     <div class="col-md-6">
-<ul class="list-group">
-  <li class="list-group-item active">Duatlón Bragado</li>
-  <li class="list-group-item">Domingo 15 Marzo Duatlón Bragado (Bragado)</li>
-  <li class="list-group-item">Domingo 19 Abril Duatlón Bragado (Cuartel)</li>
-  <li class="list-group-item">Domingo 17 Mayo Duatlón Bragado (Cuartel)</li>
-  <li class="list-group-item">Domingo 14 Junio Duatlón Bragado (Cuartel)</li>
-  <li class="list-group-item">Domingo 9 Agosto Duatlón Bragado (Bragado)</li>
-</ul>
-   </div>
+      <ul class="list-group">
+        <li class="list-group-item active">Duatlón Bragado</li>
+        <li class="list-group-item">{{-- Domingo 15 --}} Marzo Duatlón Bragado (Bragado)</li>
+        <li class="list-group-item">{{-- Domingo 19 --}} Abril Duatlón Bragado (Cuartel)</li>
+        <li class="list-group-item">{{-- Domingo 17 --}} Mayo Duatlón Bragado (Cuartel)</li>
+        <li class="list-group-item">{{-- Domingo 14 --}} Junio Duatlón Bragado (Cuartel)</li>
+        <li class="list-group-item">{{-- Domingo 9 --}} Agosto Duatlón Bragado (Bragado)</li>
+      </ul>
+    </div>
     <div class="col-md-6">
-<ul class="list-group">
-  <li class="list-group-item active">Atletismo Bragado</li>
-  <li class="list-group-item">Domingo 19 Julio 15km de Bragado al Mundo</li>
-  <li class="list-group-item">Domingo 6 Septiembre posta 2 x 4km Brgado</li>
-</ul>
-<hr>
-<ul class="list-group">
-  <li class="list-group-item active">Duatlón 5 ciudades</li>
-  <li class="list-group-item">Domingo 22 Noviembre Duatlón cinco ciudades etapa Bragado</li>
-</ul>
-   </div>   
+      <ul class="list-group">
+        <li class="list-group-item active">Atletismo Bragado</li>
+        <li class="list-group-item">{{-- Domingo 19 --}} Julio 15km de Bragado al Mundo</li>
+        <li class="list-group-item">{{-- Domingo 6 --}} Septiembre posta 2 x 4km Brgado</li>
+      </ul>
+      <hr>
+      <ul class="list-group">
+        <li class="list-group-item active">{{-- Duatlón 5 --}} ciudades</li>
+        <li class="list-group-item">{{-- Domingo 22 --}} Noviembre Duatlón cinco ciudades etapa Bragado</li>
+      </ul>
+    </div>   
   </div>
 </section>
