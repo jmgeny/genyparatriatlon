eb1f638e1bd48ca7ca694388fa46d09b9e6fb037
JuanManuelGeny <juanmanuel.geny@gmail.com>
2020-06-08 09:47:25 -0300

    Natación

--- resources/views/noticias.blade.php
+++ resources/views/noticias.blade.php
@@ -3,34 +3,49 @@
 
       <div class="col-md-4 mb-5">
         <div class="card">
-          <img class="card-img-top" src="{{ asset('img/covid-19/Covid.jpg') }}" alt="Card image">
-          <div class="card-body">
-            <h4 class="card-title">COVID-19</h4>
-            <p style="min-height: 50px" class="card-text">Situación de COVID-19 en la Región de las Américas</p>
-            <a href="https://www.paho.org/es/temas/coronavirus/enfermedad-por-coronavirus-covid-19" class="btn btn-primary" target="new">Ver Mas</a>
-          </div>
+          <a href="https://www.lanacion.com.ar/sociedad/chau-duchas-previas-como-sera-nadar-vuelta-nid2372636" target="new">            
+            <img class="card-img-top" src="{{ asset('img/covid-19/natacion.jpg') }}" alt="Natacion">
+            <div class="card-body">
+              <h4 class="card-title">Natación</h4>
+              <p style="min-height: 50px" class="card-text">Nueva normalidad: cómo será la vuelta a las piletas luego de la cuarentena por el coronavirus</p>
+            </div>
+          </a>
         </div>
       </div>
 
       <div class="col-md-4 mb-5">
         <div class="card">
-          <img class="card-img-top" src="{{ asset('img/covid-19/Olimpic.jpg') }}" alt="Card image">
-          <div class="card-body">
-            <h4 class="card-title">Olympic Org</h4>
-            <p style="min-height: 50px" class="card-text">Últimas noticias sobre el Coronavirus (COVID-19)</p>
-            <a href="https://www.olympic.org/athlete365/es/coronavirus/" class="btn btn-primary" target="new">Ver Mas</a>
-          </div>
+          <a href="https://www.paho.org/es/temas/coronavirus/enfermedad-por-coronavirus-covid-19" target="new">
+            <img class="card-img-top" src="{{ asset('img/covid-19/Covid.jpg') }}" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">COVID-19</h4>
+              <p style="min-height: 50px" class="card-text">Situación de COVID-19 en la Región de las Américas</p>
+            </div>
+          </a>
         </div>
       </div>
 
-       <div class="col-md-4 mb-5">
+      <div class="col-md-4 mb-5">
         <div class="card">
-          <img class="card-img-top" src="{{ asset('img/covid-19/Salud.jpg') }}" alt="Card image">
-          <div class="card-body">
-            <h4 class="card-title">argentina gob</h4>
-            <p style="min-height: 50px" class="card-text">Información, recomendaciones del Ministerio de Salud de la Nación y medidas de prevención.</p>
-            <a href="https://www.argentina.gob.ar/salud/coronavirus-COVID-19" class="btn btn-primary" target="new">Ver Mas</a>
-          </div>
+          <a href="https://www.olympic.org/athlete365/es/coronavirus/" target="new">
+            <img class="card-img-top" src="{{ asset('img/covid-19/Olimpic.jpg') }}" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">Olympic Org</h4>
+              <p style="min-height: 50px" class="card-text">Últimas noticias sobre el Coronavirus (COVID-19)</p>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div class="col-md-4 mb-5">
+        <div class="card">
+          <a href="https://www.argentina.gob.ar/salud/coronavirus-COVID-19" target="new">
+            <img class="card-img-top" src="{{ asset('img/covid-19/Salud.jpg') }}" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">argentina gob</h4>
+              <p style="min-height: 50px" class="card-text">Información, recomendaciones del Ministerio de Salud de la Nación y medidas de prevención.</p>
+            </div>
+          </a>
         </div>
       </div>
 
