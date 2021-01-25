16193ccabfff7e4283725d131128381ac3e37db1
JuanManuelGeny <juanmanuel.geny@gmail.com>
2021-01-25 11:11:43 -0300

    Actualizacion reloj

--- resources/views/listaCarreras.blade.php
+++ resources/views/listaCarreras.blade.php
@@ -1,69 +1,39 @@
 <section class="container">
-  <div class="row">
-    <div class="col-md-6 offset-md-3">
-      {{-- <h5 class="mt-0"><strong>Proxima Carrera</strong></h5> --}}
-      <ul class="list-goup">
-         <li class="list-group-item">
-          <div class="media">
-              <iframe id="oak-embed" style="border: 0;" src="https://embed-countdown.onlinealarmkur.com/es/#2021-04-18T00:00:00"></iframe>
-            <div class="media-body">
-              <img src="img/pampatraviesa.jpg" class="mr-3" alt="maraton">
-              <a href="https://www.facebook.com/quesossantamaria/" target="new">Maraton Pampa Traviesa</a>
-            </div>
-          </div>
-        </li> 
-        <li class="list-group-item">
-          <div class="media">
-              <iframe id="oak-embed" style="border: 0;" src="https://embed-countdown.onlinealarmkur.com/es/#2021-11-07T08:00:00"></iframe>
-            <div class="media-body">
-              <img src="img/nym50year.jpg" class="mr-3 float-left" alt="Marathon" style="max-width: 100px">
-              <a href="https://www.nyrr.org/tcsnycmarathon" target="new">TCS NY Marathon 50 year</a>
-              <p>7 de Noviembre de 2021</p>
-            </div>
-          </div>
-        </li>
-{{--         <li class="list-group-item">
-          <div class="media">
-            <div class="media-body">
-              <img src="img/duatlon.png" class="mr-3" alt="maraton">
-              <a href="#" target="new">Duatlon Bragado - a reprogramar</a>
-            </div>
-          </div>
-        </li>
-        <li class="list-group-item">
-          <div class="media">
-            <div class="media-body">
-              <img src="img/maraton.png" class="mr-3" alt="maraton">
-              <a href="https://maratondemendoza.com/" target="new">Maraton de Mendoza 10km ¿?</a>
-            </div>
-          </div>
-        </li> --}}    
-      </ul>
+
+<div class="card-deck">
+  <div class="card">
+    <div class="embed-responsive embed-responsive-1by1">
+      <iframe class="embed-responsive embed-responsive-1by1" src="https://embed-countdown.onlinealarmkur.com/es/#2021-04-18T00:00:00" allowfullscreen></iframe>
+    </div>
+    <img src="img/pampatraviesa.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Maraton Pampa Traviesa</h5>
+      <p class="card-text">Santa Rosa La Pampa</p>
+      {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
     </div>
   </div>
-  <hr>
-  <div class="row">
-    <div class="col-md-4 my-2">
-      <ul class="list-group">
-        <li class="list-group-item active">5 Ciudades</li>
-      </ul>
+  <div class="card">
+    <div class="embed-responsive embed-responsive-1by1">
+      <iframe class="embed-responsive-item" src="https://embed-countdown.onlinealarmkur.com/es/#2021-11-07T08:00:00" allowfullscreen></iframe>
     </div>
-    <div class="col-md-4 my-2">
-      <ul class="list-group">
-        <li class="list-group-item active">Duatlón Bragado</li>
-{{--         <li class="list-group-item">a reprogramar Duatlón Bragado (Bragado)</li>
-        <li class="list-group-item">Abril Duatlón Bragado (Cuartel)</li>
-        <li class="list-group-item">Mayo Duatlón Bragado (Cuartel)</li>
-        <li class="list-group-item">Junio Duatlón Bragado (Cuartel)</li>
-        <li class="list-group-item">Agosto Duatlón Bragado (Bragado)</li> --}}
-      </ul>
-    </div>    
-    <div class="col-md-4 my-2">
-      <ul class="list-group">
-        <li class="list-group-item active">Atletismo Bragado</li>
-{{--         <li class="list-group-item">Julio 15km de Bragado al Mundo</li>
-        <li class="list-group-item">Septiembre posta 2 x 4km Brgado</li> --}}
-      </ul>
-    </div>   
+    <img src="img/nym50year.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">TCS NY Marathon 50 year</h5>
+      <p class="card-text">7 de Noviembre de 2021</p>
+      {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+    </div>
   </div>
+  <div class="card">
+    <div class="embed-responsive embed-responsive-1by1">
+      <iframe class="embed-responsive-item" src="https://youtu.be/PZc2b5X_0m0" allowfullscreen></iframe>
+    </div>
+    <img src="img/GenyParatriatlonTransparente.png" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Monterrey</h5>
+      {{-- <p class="card-text">7 de Noviembre de 2021</p> --}}
+      {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
+    </div>
+  </div>  
+</div>
+<hr>
 </section>
